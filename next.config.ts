dc01ff2337888a4ebe9a359d04bf006e0cfac3fe
SquiralDot...🖤 <siaspride@gmail.com>
2026-09-24 03:42:36 +0530

    chore: restrict remote image hosts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,13 @@
 import type { NextConfig } from 'next';
-const nextConfig: NextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] } };
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'blogger.googleusercontent.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
+};
+
 export default nextConfig;
