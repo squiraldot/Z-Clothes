2ec606d4498ea5bd9dcdbdcc459457257c4dafe6
SquiralDot...🖤 <siaspride@gmail.com>
2026-09-25 15:05:22 +0530

    security(content): sanitize Blogger product markup

---
+++ lib/sanitize-html.ts
@@ -0,0 +1,13 @@
+export function sanitizeProductHtml(html: string): string {
+  return html
+    .replace(/<script\b[^>]*>[\s\S]*?<\/script>/gi, '')
+    .replace(/<style\b[^>]*>[\s\S]*?<\/style>/gi, '')
+    .replace(/<iframe\b[^>]*>[\s\S]*?<\/iframe>/gi, '')
+    .replace(/<object\b[^>]*>[\s\S]*?<\/object>/gi, '')
+    .replace(/<embed\b[^>]*>/gi, '')
+    .replace(/<form\b[^>]*>[\s\S]*?<\/form>/gi, '')
+    .replace(/\son[a-z]+\s*=\s*(['"]).*?\1/gi, '')
+    .replace(/\son[a-z]+\s*=\s*[^\s>]+/gi, '')
+    .replace(/\s(?:href|src)\s*=\s*(['"])\s*javascript:[\s\S]*?\1/gi, '')
+    .replace(/\s(?:href|src)\s*=\s*javascript:[^\s>]+/gi, '');
+}
