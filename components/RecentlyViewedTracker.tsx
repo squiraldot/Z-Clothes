b00b2c40635dfa1831a6c16bdac489ea6c097f06
SquiralDot...🖤 <siaspride@gmail.com>
2026-09-26 10:31:30 +0530

    chore: remove unstable Phase 35 components

--- components/RecentlyViewedTracker.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-import { rememberRecentlyViewed } from './RecentlyViewed';
-export function RecentlyViewedTracker({productId}:{productId:string}){useEffect(()=>{rememberRecentlyViewed(productId)},[productId]);return null;}
